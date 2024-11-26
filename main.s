cdd7fa06118bb36671a401c4b09bcb0bbf805da6
gp122 <gp122@skbl-418-012.ph.ic.ac.uk>
2024-11-26 14:00:16 +0000

    Second line on the LCD

--- main.s
+++ main.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
 
 extrn	UART_Setup, UART_Transmit_Message  ; external subroutines
-extrn	LCD_Setup, LCD_Write_Message
+extrn	LCD_Setup, LCD_Write_Message, LCD_Send_Byte_I
 	
 psect	udata_acs   ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
@@ -9,13 +9,19 @@ delay_count:ds 1    ; reserve one byte for counter in the delay routine
     
 psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
 myArray:    ds 0x80 ; reserve 128 bytes for message data
+myList:	    ds 0x80
 
 psect	data    
 	; ******* myTable, data in programme memory, and its length *****
 myTable:
-	db	'H','e','l','l','o',' ','W','o','r','l','d','!',0x0a
+	db	'H','e','l','l','o',' ','W','O','r','l','d','!',0x0a
 					; message, plus carriage return
 	myTable_l   EQU	13	; length of data
+	align	2
+	
+mySecondTable:
+	db	'S','e','c','o','n','d',' ','L','i','n','e','!',0x0a
+	mySecondTable_l	EQU 13	; length of second message
 	align	2
     
 psect	code, abs	
@@ -43,15 +49,44 @@ loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
 	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
 	decfsz	counter, A		; count down to zero
 	bra	loop		; keep going until finished
-		
+	
+	; Write first message to UART
 	movlw	myTable_l	; output message to UART
 	lfsr	2, myArray
 	call	UART_Transmit_Message
 
+	; Write first message to LCD
 	movlw	myTable_l	; output message to LCD
 	addlw	0xff		; don't send the final carriage return to LCD
 	lfsr	2, myArray
 	call	LCD_Write_Message
+	
+	; Move cursor to second line
+	movlw	0xC0		
+	call	LCD_Send_Byte_I
+	
+	; Prepare and write the second line
+	lfsr	0, myList
+	movlw	low highword(mySecondTable)
+	movwf	TBLPTRU, A
+	movlw	high(mySecondTable)
+	movwf	TBLPTRH, A
+	movlw	low(mySecondTable)
+	movwf	TBLPTRL, A
+	movlw	mySecondTable_l
+	movwf	counter, A
+
+loop2:
+	tblrd*+
+	movff	TABLAT, POSTINC0
+	decfsz	counter, A
+	bra	loop2
+	
+	; Write second message to LCD
+	movlw	mySecondTable_l
+	addlw	0xff
+	lfsr	2, myList    ; load address of the second message
+	call	LCD_Write_Message   ; write second message to the LCD
 
 	goto	$		; goto current line in code
 
