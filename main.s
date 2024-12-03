5b1aefe797a173864007c3d03b1b2785b9e3cd4b
gp122 <gp122@skbl-418-012.ph.ic.ac.uk>
2024-12-03 15:33:35 +0000

    Code cleanup Interrupt 2 Lines work rn and the arrow ish. The words overlap, probably an error with the cursor movement.

--- main.s
+++ main.s
@@ -2,22 +2,22 @@
 ;-----------------------------------------
 ; External and Global variables
 ;-----------------------------------------
-extrn	Check_Buttons, Move_Up, Move_Down, Select_Line  ; external subroutines
+extrn	Check_Buttons, Move_Up, Move_Down, Select_Line, Button_Int  ; external subroutines
 extrn	LCD_Setup, LCD_Write_Message, LCD_Send_Byte_I, LCD_Clear
 extrn	Read_Line1, Read_Line2,	Read_Arrow, Move_Line1, Move_Line2, Write_Line1, Write_Line2, Write_Arrow
-    
 
 global	counter, current_line, delay_count, myArray, FirstLine, FirstLine_l, SecondLine, SecondLine_l, Arrow, Arrow_l, Display_Menu
+    
 ;-----------------------------------------
 ; Holding space for constants
 ;-----------------------------------------
-psect	udata_acs   ; reserve data space in access ram
-counter:    ds 1    ; reserve one byte for a counter variable
-delay_count:	ds  1    ; reserve one byte for counter in the delay routine
-current_line:	ds  1	 ; current line of menu (0x80 = line 1, 0xC0 = line 2, diff = 0x40) 
+psect	udata_acs		    ; reserve data space in access ram
+counter:	ds 1		    ; reserve one byte for a counter variable
+delay_count:	ds  1		    ; reserve one byte for counter in the delay routine
+current_line:	ds  1		    ; current line of menu (0x80 = line 1, 0xC0 = line 2, diff = 0x40) 
     
-psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
-myArray:    ds 0x80 ; reserve 128 bytes for message data
+psect	udata_bank4		    ; reserve data anywhere in RAM (here at 0x400)
+myArray:    ds 0x80		    ;reserve 128 bytes for message data
 
     
 ;-----------------------------------------
@@ -30,45 +30,52 @@ FirstLine:
 	align	2
 	
 SecondLine:
-	db	'L', 'i', 'g', 'h', 't',0x0a
-	SecondLine_l	EQU 6	; length of second message
+	db	'L', 'L', 'i', 'g', 'h', 't',0x0a
+	SecondLine_l	EQU 7		; length of second message
 	align	2
 	
 Arrow:
-	db	0xFF, 0x0a  ; currently just an indicator, not an arrow
+	db	'<', 0x0a		; currently just an indicator, not an arrow
 	Arrow_l	EQU 2
 	align	2
 
 ;-----------------------------------------
-; Main Program
+; Setup Section
 ;-----------------------------------------
 psect	code, abs	
 rst: 	org 0x0
  	goto	setup
-	
-setup:	bcf	CFGS	; point to Flash program memory  
-	bsf	EEPGD 	; access Flash program memory
+
+int_hi:	org	0x0008
+	goto	Button_Int
+	    
+setup:	
+	bcf	CFGS		; point to Flash program memory  
+	bsf	EEPGD		; access Flash program memory
 	
 	movlw	0xFF
-	movwf	TRISA, A	; sets PORTB as the input
+	movwf	TRISA, A	; sets PORTA as the input
 	
 	call	LCD_Setup	; setup LCD
-	call	Initial_Menu	; inital display
+	goto	Display_Menu
+
+;-----------------------------------------
+; Interrupt Service Routine
+;-----------------------------------------
 	
-Button_Loop:
-	call	Check_Buttons
-	bra	Button_Loop
-    
+;-----------------------------------------
+; Initial Menu Display Routine; i don't think we need it
+;-----------------------------------------
 Initial_Menu:
 	call	Read_Line1
 	
-loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
+loop: 	tblrd*+				; one byte from PM to TABLAT, increment TBLPRT
+	movff	TABLAT, POSTINC0	; move data from TABLAT to (FSR0), inc FSR0	
 	decfsz	counter, A		; count down to zero
-	bra	loop		; keep going until finished
+	bra	loop			; keep going until finished
 
-	call	Write_Line1	; write first message
-	call	Move_Line2	; Move cursor to second line
+	call	Write_Line1		; write first message
+	call	Move_Line2		; Move cursor to second line
 	call	Read_Line2
 
 loop2:
@@ -80,26 +87,29 @@ loop2:
 	call	Write_Line2
 	call	Move_Line1
 	
-	return		; goto current line in code
+	return			; goto current line in code
 
+;-----------------------------------------
+; Display Routine
+;-----------------------------------------
 Display_Menu:
 	call	Read_Line1
 	call	Read_Arrow
 	
 display_loop:
-        tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
+        tblrd*+				; one byte from PM to TABLAT, increment TBLPRT
+	movff	TABLAT, POSTINC0	; move data from TABLAT to (FSR0), inc FSR0	
 	decfsz	counter, A		; count down to zero
 	bra	display_loop		; keep going until finished
 
-	call	Write_Line1	; write first message
+	call	Write_Line1	    ; write first message
 	
 	movf    current_line, W     ; Move current_line value to W
 	sublw   1                   ; Subtract W from 1 (checking if current_line == 1)
 	btfss   STATUS, 2           ; Skip next instruction if not zero
 	call    Write_Arrow         ; Call if current_line == 1
 	
-	call	Move_Line2	; Move cursor to second line
+	call	Move_Line2	    ; Move cursor to second line
 	call	Read_Line2
 
 display_loop2:
@@ -121,7 +131,7 @@ display_loop2:
 ;-----------------------------------------
 ; Delay
 ;-----------------------------------------
-delay:	decfsz	delay_count, A	; decrement until zero
+delay:	decfsz	delay_count, A	    ; decrement until zero
 	bra	delay
 	return
 
