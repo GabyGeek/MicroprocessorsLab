0498238b9d921421a7f94151faf090b053b383d6
gp122 <gp122@skbl-418-012.ph.ic.ac.uk>
2024-12-05 16:00:47 +0000

    deleting the button interrupt and delays in check buttons

--- Buttons.s
+++ Buttons.s
@@ -1,17 +1,12 @@
 #include <xc.inc>
     
-extrn	current_line
+extrn	current_line, delay_count
 extrn	Display_Menu, LCD_Clear
 global	Check_Buttons, Move_Up, Move_Down, Select_Line, Button_Int
     
  
 Button_Int:
-	btfss	TMR0IF		; check that this is timer0 interrupt
-	retfie	f		; if not then return
-	incf	LATC, F, A	; increment PORT
-	bcf	TMR0IF		; clear interrupt flag
-	retfie	f		; fast return from interrupt
-	return
+    return
     
 Check_Buttons:
     btfsc   PORTC, 0, A		; If bit 0 of PORTB is clear, skip the command
@@ -29,19 +24,20 @@ Move_Up:
    ; if on line 2, make current line 0
    movlw    0
    movwf    current_line
-   goto	    Display_Menu
    return
 
 Move_Down:
     ; if on line 1, make current line 1
    movlw    1
    movwf    current_line
-   goto	    Display_Menu
    return
 
     
 Select_Line:
-    call LCD_Clear		; Currently just clears the LCD display
+    ;call LCD_Clear		; Currently just clears the LCD display
     return
 
+delay:	decfsz	delay_count, A	    ; decrement until zero
+	bra	delay
+	return
 
