3984aab24a5bf18f6ebe381a67ce556cf0f2b38a
GabyGeek <gabriela@pyka.co.uk>
2024-12-12 11:01:53 +0000

    Idk what changes happened here...Debouncing i think

--- Buttons.s
+++ Buttons.s
@@ -5,13 +5,13 @@ extrn	Display_Menu, LCD_Clear
 global	Check_Buttons, Move_Up, Move_Down, Select_Line
     
 Check_Buttons:
-    btfsc   PORTC, 0, A		; If bit 0 of PORTB is clear, skip the command
+    btfsc   PORTD, 0, A		; If bit 0 of PORTB is clear, skip the command
     call    Move_Up
     
-    btfsc   PORTC, 1, A
+    btfsc   PORTD, 1, A
     call    Move_Down
     
-    btfsc   PORTC, 2, A
+    btfsc   PORTD, 2, A
     call    Select_Line
     
     return
@@ -19,19 +19,28 @@ Check_Buttons:
 Move_Up:
    ; if on line 2, make current line 0
    movlw    0
-   movwf    current_line
+   movwf    current_line, A
+   movlw    200		    ; delay for debouncing of the button
+   movwf    delay_count, A
+   call	    delay
    return
 
 Move_Down:
     ; if on line 1, make current line 1
    movlw    1
-   movwf    current_line
+   movwf    current_line, A
+   movlw    200		    ; delay for debouncing of the button
+   movwf    delay_count, A
+   call	    delay
    return
 
     
 Select_Line:
     ;call LCD_Clear		; Currently just clears the LCD display
-    return
+   movlw    200		    ; delay for debouncing of the button
+   movwf    delay_count, A
+   call	    delay
+   return
 
 delay:	decfsz	delay_count, A	    ; decrement until zero
 	bra	delay
