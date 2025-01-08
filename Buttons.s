6dd9f269374713f9b8ca63950d6eba887d2265a3
Gabriela Pyka <gabriela@pyka.co.uk>
2025-01-08 14:39:15 +0000

    changed to 4 buttons

--- Buttons.s
+++ Buttons.s
@@ -2,22 +2,9 @@
     
 extrn	current_line, delay_count
 extrn	Display_Menu, LCD_Clear
-global	Check_Buttons, Move_Up, Move_Down, Select_Line
-    
-Check_Buttons:
-    btfsc   PORTD, 0, A		; If bit 0 of PORTB is clear, skip the command
-    call    Move_Up
-    
-    btfsc   PORTD, 1, A
-    call    Move_Down
-    
-    btfsc   PORTD, 2, A
-    call    Select_Line
-    
-    return
-    
-Move_Up:
-   ; if on line 2, make current line 0
+global	Arrow_to_Temperature, Arrow_to_Light, Arrow_to_Moisture
+
+Arrow_to_Temperature:
    movlw    0
    movwf    current_line, A
    movlw    0XFF		    ; delay for debouncing of the button
@@ -25,8 +12,7 @@ Move_Up:
    call	    delay
    return
 
-Move_Down:
-    ; if on line 1, make current line 1
+Arrow_to_Light:
    movlw    1
    movwf    current_line, A
    movlw    0xFF		    ; delay for debouncing of the button
@@ -34,12 +20,10 @@ Move_Down:
    call	    delay
    return
 
-    
-Select_Line:
-    ;call LCD_Clear		; Currently just clears the LCD display
+Arrow_to_Moisture:
    movlw    2
    movwf    current_line, A
-   movlw    0XFF		    ; delay for debouncing of the button
+   movlw    0xFF		    ; delay for debouncing of the button
    movwf    delay_count, A
    call	    delay
    return
